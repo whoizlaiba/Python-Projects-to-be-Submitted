65e1c016e81f04fdeab7aec88aabefe47c658d19
Laiba Parvez <164337174+whoizlaiba@users.noreply.github.com>
2025-02-25 00:55:44 +0500

    Add files via upload

---
+++ Mad libs Python Project/Project_1.py
@@ -0,0 +1,15 @@
+Your_Name = input("My name is: ")
+Select_Education_Board = input("Select your education board: ")
+Skill_1 = input("Add your skill name: ")
+Skill_2 = input("Add your skill name: ")
+Skill_3 = input("Add your skill name: ")
+Skill_4 = input("Add your skill name: ")
+Skill_5 = input("Add your skill name: ")
+Recently_Learning = input("What are you learning recently? ")
+
+Sentence = f"I’m {Your_Name}, an intermediate student at the {Select_Education_Board} board, passionate about frontend development and web technologies. With expertise in {Skill_1}, {Skill_2}, {Skill_3}, {Skill_4}, and {Skill_5}, I focus on creating responsive, user-friendly web applications. \
+Currently, I’m honing my skills through the Governor Sindh’s IT Initiative, where I’m diving deep into modern web technologies such as HTML, CSS, JavaScript, TypeScript, and Next.js, among others. This hands-on experience is equipping me with the tools to not only understand but also create dynamic, future-ready web solutions. I’m driven by a desire to innovate and continuously expand my knowledge in the ever-evolving world of IT. \
+Additionally, I’m learning {Recently_Learning} to enhance my development capabilities and broaden my technical expertise. I enjoy solving problems, building efficient web solutions, and continuously improving my craft. \
+I’m excited to connect with like-minded professionals, collaborate on projects, and grow in the tech industry!"
+
+print(Sentence)
